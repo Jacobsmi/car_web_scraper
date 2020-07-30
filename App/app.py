35fea7eb1cdc3539eae891cafbb6c4bb33ad95a2
Jacobsmi <34344408+Jacobsmi@users.noreply.github.com>
2020-07-30 07:33:35 -0400

    Basic features implemented

--- App/app.py
+++ App/app.py
@@ -15,27 +15,52 @@ class App:
 
         self.web_scraper = Scraper()
         # Call the get_car_page which opens the webpage and scrapes for the information
-        query_result = self.web_scraper.get_car_page(self.user_car.CAR_MAKE, self.user_car.CAR_MODEL, self.user_car.ZIP_CODE)
+        query_result = self.web_scraper.get_car_page(
+            self.user_car.CAR_MAKE, self.user_car.CAR_MODEL, self.user_car.ZIP_CODE)
         if type(query_result) is str:
             print(f"ERROR: {query_result}")
         else:
-            self.find_matching_cars()
+            matching_cars = self.find_matching_cars()
+        self.print_matching_cars(matching_cars)
+
+    def print_matching_cars(self, matching_cars):
+        x = 1
+        for car in matching_cars:
+            print("CAR #{}:".format(x))
+            print("YEAR: {}".format(matching_cars[0]))
+            print("MAKE AND MODEL: {} {}".format(matching_cars[1], matching_cars[2]))
+            print("PRICE: {}".format(matching_cars[4]))
+            print("MILEAGE: {}".format(matching_cars[5]))
+
 
     def find_matching_cars(self):
+        matching_cars = []
         for car in self.web_scraper.CARS:
             index = self.web_scraper.CARS.index(car)
-            try:
-                year_num = int(car[0])
-                if year_num < self.user_params.EARLIEST_YEAR or year_num > self.user_params.LATEST_YEAR:
-                    print("This car should be removed {} {} {}".format(self.user_params.EARLIEST_YEAR,self.user_params.LATEST_YEAR,car))
-                    self.web_scraper.CARS.pop(index)
-                # if year_num <= self.user_params.LATEST_YEAR and year_num >= self.user_params.EARLIEST_YEAR:
-                #     print("Within the years {} {} {}".format(self.user_params.LATEST_YEAR,self.user_params.LATEST_YEAR,car))
-                # elif self.user_params.EARLIEST_YEAR == -1 and self.user_params.LATEST_YEAR == -1:
-                #     print("Within the years {} {} {}".format(self.user_params.LATEST_YEAR,self.user_params.LATEST_YEAR,car))
 
-            except:
-                print("Error converting to num")
-        print("The cars left are")
-        for car in self.web_scraper.CARS:
-            print(car)
+            year_num = int(car[0])
+            cost_num = int(car[4])
+            mileage_num = int(car[5])
+            # NEED A WAY TO CHECK FOR NEGATIVE 1
+            correct_year_value = self.check_if_fit(
+                year_num, self.user_params.EARLIEST_YEAR, self.user_params.LATEST_YEAR)
+            correct_price_value = self.check_if_fit(
+                cost_num, self.user_params.LOW_PRICE, self.user_params.HIGH_PRICE)
+            correct_mileage_value = self.check_if_fit(
+                mileage_num, self.user_params.LOW_MILEAGE, self.user_params.HIGH_MILEAGE)
+            #print("CAR: {} \nCORRECT YEAR: {}\nCORRECT PRICE: {}\nCORRECT MILEAGE: {}\n".format(car, correct_year_value, correct_price_value, correct_mileage_value))
+            # NOT SURE IF THINGS ARE BEING PROPERLY ADDED MORE TESTING IS NEEDED
+            if (correct_year_value == True and correct_price_value == True) and correct_mileage_value == True:
+                    matching_cars.append(car)
+        return matching_cars
+
+    def check_if_fit(self, value, low_expected, high_expected):
+        if low_expected == -1 and high_expected == -1:
+            return True
+        elif low_expected == -1 and (high_expected != -1 and value < high_expected):
+            return True
+        elif (low_expected != -1 and value > low_expected) and high_expected == -1:
+            return True
+        elif value < low_expected or value > high_expected:
+            return False
+        return True
